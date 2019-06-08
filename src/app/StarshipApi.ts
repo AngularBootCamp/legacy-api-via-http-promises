a452813488fa95e546edf651755df04caec5da7e
Kyle Cordes <kyle.cordes@oasisdigital.com>
2019-06-08 06:45:46 -0500

    curriculum update

--- src/app/StarshipApi.ts
+++ src/app/StarshipApi.ts
@@ -7,20 +7,29 @@ import { jsonRequestHeaders } from './httpUtils';
   providedIn: 'root'
 })
 export class StarshipApi {
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   loadStarships(): Promise<any> {
-    return this.http.get<any>('https://swapi.co/api/starships/', {headers: jsonRequestHeaders})
+    return this.http
+      .get<any>('https://swapi.co/api/starships/', {
+        headers: jsonRequestHeaders
+      })
       .toPromise(Promise)
-      .then(shipList => {  // Promise-based APIs still work fine:
-        console.log('Ship list retrieved, GETting film data', shipList);
+      .then(shipList => {
+        // Promise-based APIs still work fine:
+        console.log(
+          'Ship list retrieved, GETting film data',
+          shipList
+        );
         return Promise.all(
           shipList.results.map((ship: any) => {
             console.log('GETting film data for ' + ship.name);
-            return this.http.get<any>(ship.films[0], {headers: jsonRequestHeaders})
+            return this.http
+              .get<any>(ship.films[0], {
+                headers: jsonRequestHeaders
+              })
               .toPromise(Promise)
-              .then((film) => {
+              .then(film => {
                 // console.log(film);
                 ship.filmName = film.title;
                 return ship;
--- src/app/starshipList.ts
+++ src/app/starshipList.ts
@@ -10,8 +10,9 @@ export class StarshipListComponent {
   starships: any[] = [];
 
   constructor(api: StarshipApi) {
-    api.loadStarships()
-      .then((data: any) => this.starships = data)
-      .catch(_err => this.starships = []);
+    api
+      .loadStarships()
+      .then((data: any) => (this.starships = data))
+      .catch(_err => (this.starships = []));
   }
 }
