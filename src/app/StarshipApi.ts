e6ddc2b45327a2c131df4404e54d281129960c61
Kyle Cordes <kyle.cordes@oasisdigital.com>
2022-04-11 18:28:58 -0500

    curriculum update

--- src/app/StarshipApi.ts
+++ src/app/StarshipApi.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
 import { jsonRequestHeaders } from './httpUtils';
 
@@ -10,32 +11,27 @@ export class StarshipApi {
   constructor(private http: HttpClient) {}
 
   loadStarships(): Promise<any> {
-    return this.http
-      .get<any>('https://swapi.co/api/starships/', {
+    return firstValueFrom(
+      this.http.get<any>('https://swapi.co/api/starships/', {
         headers: jsonRequestHeaders
       })
-      .toPromise(Promise)
-      .then(shipList => {
-        // Promise-based APIs still work fine:
-        console.log(
-          'Ship list retrieved, GETting film data',
-          shipList
-        );
-        return Promise.all(
-          shipList.results.map((ship: any) => {
-            console.log('GETting film data for ' + ship.name);
-            return this.http
-              .get<any>(ship.films[0], {
-                headers: jsonRequestHeaders
-              })
-              .toPromise(Promise)
-              .then(film => {
-                // console.log(film);
-                ship.filmName = film.title;
-                return ship;
-              });
-          })
-        );
-      });
+    ).then(shipList => {
+      // Promise-based APIs still work fine:
+      console.log('Ship list retrieved, GETting film data', shipList);
+      return Promise.all(
+        shipList.results.map((ship: any) => {
+          console.log('GETting film data for ' + ship.name);
+          return firstValueFrom(
+            this.http.get<any>(ship.films[0], {
+              headers: jsonRequestHeaders
+            })
+          ).then(film => {
+            // console.log(film);
+            ship.filmName = film.title;
+            return ship;
+          });
+        })
+      );
+    });
   }
 }
